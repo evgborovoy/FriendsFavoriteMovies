ddd12503fccd6a4402f098c1c5d7570ad3cf21fd
Evgeniy <borovoyevgen@gmail.com>
2024-11-23 22:28:21 +0400

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Evgeniy Borovoy on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
